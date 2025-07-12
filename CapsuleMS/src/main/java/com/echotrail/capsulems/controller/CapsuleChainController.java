e064f396d1eb7770a8b20224027b7c66cf0b7444
santhoshkumarCodes <santhoshkumartechsavvy@gmail.com>
2025-07-12 21:34:48 +0530

    CapsuleChainController: Refactor DTOs to make the API contract clear
    This commit creates seperate DTOs for linkNextRequest and linkPreviousRequest to make the API contract clear.

--- CapsuleMS/src/main/java/com/echotrail/capsulems/controller/CapsuleChainController.java
+++ CapsuleMS/src/main/java/com/echotrail/capsulems/controller/CapsuleChainController.java
@@ -1,6 +1,8 @@
 package com.echotrail.capsulems.controller;
 
 import com.echotrail.capsulems.DTO.CapsuleChainDTO;
+import com.echotrail.capsulems.DTO.LinkNextRequest;
+import com.echotrail.capsulems.DTO.LinkPreviousRequest;
 import com.echotrail.capsulems.service.CapsuleChainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -35,13 +37,13 @@ public class CapsuleChainController {
     }
 
     @PutMapping("/link/previous")
-    public ResponseEntity<Void> setPreviousCapsule(@RequestBody CapsuleChainDTO linkDTO, @RequestHeader("X-UserId") Long userId) {
+    public ResponseEntity<Void> setPreviousCapsule(@RequestBody LinkPreviousRequest linkDTO, @RequestHeader("X-UserId") Long userId) {
         capsuleChainService.setPreviousCapsuleId(linkDTO.getCapsuleId(), linkDTO.getPreviousCapsuleId(), userId);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/link/next")
-    public ResponseEntity<Void> setNextCapsule(@RequestBody CapsuleChainDTO linkDTO, @RequestHeader("X-UserId") Long userId) {
+    public ResponseEntity<Void> setNextCapsule(@RequestBody LinkNextRequest linkDTO, @RequestHeader("X-UserId") Long userId) {
         capsuleChainService.setNextCapsuleId(linkDTO.getCapsuleId(), linkDTO.getNextCapsuleId(), userId);
         return ResponseEntity.ok().build();
     }
